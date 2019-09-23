5bb5862c763fc38ce402c5f640734518fab2d502
ossama <osama.mum.mscs@gmail.com>
2019-09-23 18:19:54 -0500

    adding test class for DB creation at first

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,21 @@
+package test;
+
+import edu.mum.wap.models.Post;
+import edu.mum.wap.models.User;
+import edu.mum.wap.services.PostService;
+import edu.mum.wap.services.UserService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        User user = new User();
+        user.setUsername("ossama");
+        userService.addUser(user);
+        Post post  = new Post();
+        post.setDescription("desssssssssssc");
+        post.setUserId(user);
+        PostService postService = new PostService();
+        postService.addPost(post);
+    }
+}
