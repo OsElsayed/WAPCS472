d7fd42d8700e4c9b311d4dadf8e4e06c1a6d0baf
widjesh <widjesh@satyamholidays.com>
2019-09-23 11:03:05 -0500

    rename fonts V2

--- src/main/java/edu.mum.wap/servlets/TimelineServlet.java
+++ src/main/java/edu.mum.wap/servlets/TimelineServlet.java
@@ -7,21 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/Timeline")
+@WebServlet("/pages/Timeline")
 public class TimelineServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
-        String comment = req.getParameter("comment");
-//        Part filepart = req.getPart("photo_file");
-        System.out.println(comment);
-        PrintWriter pw = resp.getWriter();
-        pw.write(comment);
+        String comment = req.getParameter("information");
+        Part part = req.getPart("photo");
+        String filename = part.getSubmittedFileName();
+        resp.getWriter().write(comment);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,15 @@
 <html>
+<head>
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/timeline.js"></script>
+</head>
 <body>
 <h2>Hello World!</h2>
 <div>
-
+<input type="button" id="newid" value="sdsd">
 </div>
 </body>
 </html>
