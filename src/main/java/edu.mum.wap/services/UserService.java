2d68593995efe8b12df2fcdfd883373df89150b8
widjesh <widjesh@satyamholidays.com>
2019-09-22 20:33:55 -0500

    Ajax Testing

--- src/main/java/edu.mum.wap/services/UserService.java
+++ src/main/java/edu.mum.wap/services/UserService.java
@@ -1,4 +1,9 @@
 package edu.mum.wap.services;
 
+import edu.mum.wap.models.User;
+
 public class UserService {
+    public User GetUser(int id){
+        return new User();
+    }
 }
--- src/main/java/edu.mum.wap/servlets/TimelineServlet.java
+++ src/main/java/edu.mum.wap/servlets/TimelineServlet.java
@@ -1,7 +1,27 @@
 package edu.mum.wap.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("timeline")
-public class TimelineServlet {
+@WebServlet("/Timeline")
+public class TimelineServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        String comment = req.getParameter("comment");
+//        Part filepart = req.getPart("photo_file");
+        System.out.println(comment);
+        PrintWriter pw = resp.getWriter();
+        pw.write(comment);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
 }
