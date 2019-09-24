7aa22e65e1b18baf1a719e4c389e71791c6418d9
Mohammad Moharram <mammoharram@hotmail.com>
2019-09-24 17:58:23 -0500

    newww

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,27 +1,29 @@
 package test;
 
 import edu.mum.wap.models.Post;
-import edu.mum.wap.models.User;
 import edu.mum.wap.services.PostService;
-import edu.mum.wap.services.UserService;
 
-import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        UserService userService = new UserService();
-        User user = new User();
-        user.setUsername("ossama");
-        try {
-            userService.addUser(user);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        Post post  = new Post();
-        post.setDescription("desssssssssssc");
-        post.setUserId(1);
-        PostService postService = new PostService();
-        postService.addPost(post);
+//        UserService userService = new UserService();
+//        User user = new User();
+//        user.setUsername("ossama");
+//        try {
+//            userService.addUser(user);
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//        Post post  = new Post();
+//        post.setDescription("desssssssssssc");
+//        post.setUserId(1);
+//        PostService postService = new PostService();
+//        postService.addPost(post);
+
+        PostService psv = new PostService();
+        List<Post> p =psv.getMyPosts(2);
+        System.out.print(p);
     }
 }
