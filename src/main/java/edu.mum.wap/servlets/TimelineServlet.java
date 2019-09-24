f62df76f62d063aab848d77b13a2bf8f8fcd28ca
widjesh <widjesh@satyamholidays.com>
2019-09-24 13:31:34 -0500

    Posting an post with image and without image

--- src/main/java/edu.mum.wap/servlets/TimelineServlet.java
+++ src/main/java/edu.mum.wap/servlets/TimelineServlet.java
@@ -1,35 +1,60 @@
 package edu.mum.wap.servlets;
 
 import com.google.gson.Gson;
+import edu.mum.wap.models.Images;
+import edu.mum.wap.models.Post;
+import edu.mum.wap.services.ImageService;
+import edu.mum.wap.services.PostService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.*;
 
 @MultipartConfig
 @WebServlet("/pages/Timeline")
 public class TimelineServlet extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        HttpSession session = req.getSession();
+        List<Post> posts = (List<Post>) session.getAttribute("posts");
         String comment = req.getParameter("comment");
-        Part photo = req.getPart("photo");
+        String photo = req.getParameter("photo");
 
-        //PostService postService = new PostService();
-        //Post post = postService.add(comment, photo);
-        Gson gson = new Gson();
-        //String postJson = gson.toJson(post);
-        resp.setContentType("application/json");
-        //resp.getWriter().write(postJson);
+        //Instantiating Objects
+        PostService postService = new PostService();
+        Post post = new Post();
+        post.setDescription(comment);
+        post.setActive(true);
+        post.setVisible(true);
+        Date today = Calendar.getInstance().getTime();
+        post.setCreationDate(today);
+        if(photo != ""){
+            ImageService imageService = new ImageService();
+            Images image = new Images();
+            image.setImageUrl(photo);
+            post.setImages(image);
+            imageService.addImage(image);
+        }
+        postService.addPost(post);
+        posts.add(post);
+        Collections.reverse(posts);
+        session.setAttribute("posts",posts);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        List<Post> posts = new ArrayList();
+        PostService postService = new PostService();
+        posts = postService.findAll();
+        Collections.reverse(posts);
+        HttpSession session = req.getSession();
+        session.setAttribute("posts",posts);
+        RequestDispatcher rd = req.getRequestDispatcher("home.jsp");
+        rd.forward(req,resp);
     }
 }
