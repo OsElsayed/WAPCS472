51ef2234683dd64553c4fb9cf87b226f1d1586b3
C. Hoffmann <caiohoffmann@gmail.com>
2019-09-24 13:16:10 -0500

    Scripts/Css Bug 2

--- src/main/java/filters/AppFilter.java
+++ src/main/java/filters/AppFilter.java
@@ -21,16 +21,11 @@ public class AppFilter implements Filter {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
         String path = request.getRequestURI();
-        if (path.startsWith("/js/") || path.startsWith("/css/") || path.startsWith("/lib/") || path.startsWith("/fonts/")
-                || path.startsWith("/images/") || path.startsWith("/vendor/")) {
+        if (path.contains("/js/") || path.contains("/css/") || path.contains("/lib/") || path.contains("/fonts/")
+                || path.contains("/images/") || path.contains("/vendor/")) {
             chain.doFilter(request, response); // Just continue chain.
-        } else {
-            // Do your business stuff here for all paths other than /specialpath.
+            return;
         }
-        //if (request.getServletPath().contains("/sign-in")) {
-       //     chain.doFilter(request, response);
-            //return;
-       // }
 
         if (user == null && (!request.getServletPath().contains("sign-in"))
                 && (!request.getServletPath().contains("login"))){
