93949410bc03c56b0f3358e1196e9c16f95a8b86
Mohammad Moharram <mammoharram@hotmail.com>
2019-09-24 13:35:50 -0500

    Moharram - preProfile

--- src/main/java/edu.mum.wap/servlets/ProfileServlet.java
+++ src/main/java/edu.mum.wap/servlets/ProfileServlet.java
@@ -1,4 +1,48 @@
 package edu.mum.wap.servlets;
+import edu.mum.wap.models.Post;
+import edu.mum.wap.models.User;
+import edu.mum.wap.services.PostService;
 
-public class ProfileServlet {
-}
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+@WebServlet("/Profile")
+public class ProfileServlet extends HttpServlet {
+    User me;
+    PostService postSvc;
+
+    public ProfileServlet (){
+        me = new User();
+        postSvc = new PostService();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Post> myPosts =  postSvc.getMyPosts(me.getId());
+        req.getSession().setAttribute("posts",myPosts);
+
+        List<User> followings = me.getFollowersList();
+        req.getSession().setAttribute("followings",followings);
+        // retrun all posts and loop over them to create div "post-bar" for each post
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Post p = new Post();
+        p.setDescription(req.getParameter("description"));
+//        p.setImageId(Long.parseLong(req.getParameter("image")));
+        p.setUserId(me);
+        p.setActive(true);
+        p.setCreationDate((new Date()));
+        p.setVisible(true);
+        //p.setId(0);
+
+        //int result = postSvc.addPost(p);
+        postSvc.addPost(p);
+
+        // append new post to the old posts in the top of the page
+    }}
---
+++ src/main/webapp/js/post.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $.post("")
+        .done(function (data) {
+
+        })
+        .fail(function (err) {
+            console.error(err);
+        });
+})
