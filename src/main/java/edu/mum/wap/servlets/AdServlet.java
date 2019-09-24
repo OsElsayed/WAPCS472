a722baea4b15ae686e2e578ea951f4c387db338a
ossama <osama.mum.mscs@gmail.com>
2019-09-24 18:48:08 -0500

    Advertisement page

--- src/main/java/edu/mum/wap/servlets/AdServlet.java
+++ src/main/java/edu/mum/wap/servlets/AdServlet.java
@@ -1,5 +1,6 @@
 package edu.mum.wap.servlets;
 
+import edu.mum.wap.models.Advertisement;
 import edu.mum.wap.services.AdService;
 
 import javax.servlet.ServletException;
@@ -7,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 
@@ -17,19 +19,29 @@ public class AdServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
         String advUrl = req.getParameter("advUrl");
         String startStr = req.getParameter("start");
         String endStr = req.getParameter("end");
         if (advUrl == null || advUrl.isEmpty() || startStr == null || startStr.isEmpty()
                 || endStr == null || endStr.isEmpty()) {
-            resp.setStatus(500);
-            resp.getWriter().print("{description:'You need to fill all the required fields in the to register!'}");
+            session.setAttribute("error", "description:'You need to fill all the required fields in the to register!'");
             return;
         }
-        LocalDate start = LocalDate.parse(req.getParameter("start"));
-        LocalDate end = LocalDate.parse(req.getParameter("end"));
-
+        Advertisement advertisement = prepareAdvertisement(advUrl, startStr, endStr);
+        adService.addAdvertisement(advertisement);
         resp.sendRedirect("home.jsp");
-        //adService.addAdvertisement();
+    }
+
+    private Advertisement prepareAdvertisement(String advUrl, String startStr, String endStr) {
+        LocalDate start = LocalDate.parse(startStr);
+        LocalDate end = LocalDate.parse(endStr);
+        Advertisement advertisement = new Advertisement();
+        advertisement.setAdURL(advUrl);
+        java.util.Date startDate = java.sql.Date.valueOf(start);
+        java.util.Date endDate = java.sql.Date.valueOf(end);
+        advertisement.setShowingStartDate(startDate);
+        advertisement.setShowingEndDate(endDate);
+        return advertisement;
     }
 }
