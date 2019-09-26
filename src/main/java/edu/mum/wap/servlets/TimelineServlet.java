a463d2a84271289962438f160bc74cbeaab9ba79
widjesh <widjesh@satyamholidays.com>
2019-09-26 00:21:08 -0500

    Correction on Post Bug

--- src/main/java/edu/mum/wap/servlets/TimelineServlet.java
+++ src/main/java/edu/mum/wap/servlets/TimelineServlet.java
@@ -75,7 +75,7 @@ public class TimelineServlet extends HttpServlet {
             });
         }
         if(!me.isAdmin()) {
-            filtered = posts.stream().filter(p -> me.getFollowersList().contains(p.getUserId())).collect(Collectors.toList());
+            filtered = posts.stream().filter(p -> me.getFollowersList().contains(p.getUserId())|| me.getId() == p.getUserId()).collect(Collectors.toList());
         }else{
             filtered = posts;
         }
